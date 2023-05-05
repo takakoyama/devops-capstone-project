2ccbdd26fd685d97322c2678999790ee31efbfef
takakoyama <tkoyama@us.ibm.com>
2023-05-05 08:46:24 -0400

    Added CORS header

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -210,3 +210,9 @@ class TestAccountService(TestCase):
 
         for key, value in headers.items():
             self.assertEqual(resp.headers.get(key),value)
+    
+    def test_cor(self):
+        """It should be testing COR"""
+        resp = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.headers.get('Access-Control-Allow-Origin'), '*')
